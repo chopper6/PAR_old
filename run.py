c4fe27f8adde02cfb7f58e4de61825c520aef6d0
Samuel <samuel.nalin@gmail.com>
2020-02-29 18:08:42 +0100

    small fix

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from copy import deepcopy
 
 params = ({'experiment':'hist', 'repeats':10, 'time':100, 'timestamp':util.timestamp(), 
 	'NAD':1000, 'PARG':200, 'DNA': 20, 'PARP':20,
-	'base_fwd':1.0E+8, 'base_rev':1.0E-2, 'catalyze_rate':1.0E+8, 'cut_rate':1.0E-8, 'elong_boost':1.0E+1,
+	'base_fwd':1.0E+8, 'base_rev':1.0E-2, 'catalysis_rate':1.0E+8, 'cut_rate':1.0E-8, 'elong_boost':1.0E+1,
 	'out_dir':'./output/', 'write_params_on_img':True, 'save_fig':False, 'dpi':300, 'std_devs':3})
 
 
@@ -89,8 +89,8 @@ def run_sim(params):
 	for species in ['NAD','DNA','PARP','PARG']:
 		model = model.replace("init: _ " + species, "init: " + str(params[species]) + " " + species)
 
-	for rate in ['base_rate','base_fwd','catalysis_rate','cut_rate','elong_boost']:
-		model = model.replace("'" + rate + "' _", "'" + rate + "' " + params[rate])
+	for rate in ['base_rev','base_fwd','catalysis_rate','cut_rate','elong_boost']:
+		model = model.replace("'" + rate + "' _", "'" + rate + "' " + str(params[rate]))
 
 	model = model.replace("mod: ([E] [mod] _ )=0", "mod: ([E] [mod] " + str(params['time']) + " )=0")
 
