1c5927dd3ee7685c25834bcffdbe02908829842c
chopper <corbin_hopper@yahoo.com>
2020-03-05 19:43:52 +0100

    params

--- params.py
+++ params.py
@@ -1,6 +1,6 @@
 import util 
 
-params = ({'experiment':'sweep', 'repeats':8, 'time':1000, 'timestamp':util.timestamp(), 
+params = ({'experiment':'sweep', 'repeats':8, 'time':10000, 'timestamp':util.timestamp(), 
 	'NAD':10000, 'PARG':0, 'DNA': 20, 'PARP':20,
 	'base_fwd':1.0E+4, 'base_rev':1.0E-2, 'catalysis_rate':1.0E+8, 'cut_rate':1.0E+4,
 	'out_dir':'./output/local/', 'write_params_on_img':True, 'save_fig':True, 'dpi':100, 'std_devs':1})
